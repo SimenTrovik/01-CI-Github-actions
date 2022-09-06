221516aa725b4222833627a451c9421bfb607ddd
EC2 Default User <ec2-user@ip-172-31-35-158.eu-west-1.compute.internal>
2022-09-06 09:45:56 +0000

    fixed the stuff

--- src/main/java/com/pgr301/exam/BankAccountApplication.java
+++ src/main/java/com/pgr301/exam/BankAccountApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankAccountApplication {
     public static void main(String[] args) {
-lololloll
         SpringApplication.run(BankAccountApplication.class, args);
     }
 }
